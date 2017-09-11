82c34feea0a330f4b13a1164e74bd3b0a53219da
Eric Billingsley <ebilling@babrains.com>
2017-09-11 10:52:45 -0700

    Added usage statement.

--- main.go
+++ main.go
@@ -7,13 +7,16 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: pi-go-homekit CONFIG")
+	}
 	ppc := NewPoolPumpController(os.Args[1])
 	ppc.Start()
 
 	transport, err := hc.NewIPTransport(hc.Config{Pin: ppc.pin},
+		ppc.thermometer.Accessory,
 		ppc.pump.Accessory,
-		ppc.sweep.Accessory,
-		ppc.thermometer.Accessory)
+		ppc.sweep.Accessory)
 
 	if err != nil {
 		log.Fatal(err)
